356f0d2cfaca52d6aee1bd4a66a7c19730a84c7a
mihai.ciorobitca <mihai.ciorobitca@outlook.com>
2024-10-23 13:22:00 +0300

    m

--- api/main.py
+++ api/main.py
@@ -9,17 +9,27 @@ from requests import get
 
 # app = FastAPI()
 
-def getCharacters():
-    response = get("https://api.github.com/repos/mihai-ciorobitca/captcha-api/contents/api/data")
-    characters = {}
-    print(response.json)
-    for file in response.json():
-        if file["name"].endswith(".png"):
-            character = get(file["download_url"])
-            characters[b64encode(character.content).decode("utf-8")] = file["name"].replace(".png", "")
-    return characters
+characters = {
+    'iVBORw0KGgoAAAANSUhEUgAAABYAAAAnCAAAAAAu+efvAAAAYUlEQVR4nO2TMRaAMAhDE+5/ZxyQSiH6HB1kgg8NDCmwwv3KeaIGuMNENig8Wl7EsmA292HrNArrNF7YQOsSEaY0nqZ//AnsGr8UcUhrAqCx83SqduwUobQmcft36q4idgD89xdAUdWvkgAAAABJRU5ErkJggg==': '0',
+    'iVBORw0KGgoAAAANSUhEUgAAAAgAAAAnCAAAAAAXnla0AAAAKklEQVR4nM3OIQ4AIAADsY7//3kIQrA4mKqYuBRIoBiQDa+RPzIO1np3ngvcA0uhzvuwAAAAAElFTkSuQmCC': '1',
+    'iVBORw0KGgoAAAANSUhEUgAAABUAAAAnCAAAAADFzlzsAAAAfklEQVR4nLXTQQ4EIQhE0V/c/841m2lEYNGbdmN8IWUiKMA8S2U/+ojO0YdFWwYRXQWeCrCoVgX8NuErNehlrTc16+uARq23d/jjrdmqnquhzobFhkVr/6OihlZM9TUaMUNTr9Caq1HrPm+xITFuOrl9XmMrRRW1amKMLwDwA1YoGUmiQcGlAAAAAElFTkSuQmCC': '2',
+    'iVBORw0KGgoAAAANSUhEUgAAABMAAAAnCAAAAADI0CyrAAAAXklEQVR4nM2SMQ7AIAwDz6j//7I7IAIkdKYMQRy2iExkYmnsCxt83OEJtcsMohWrM9veWt0n3SWmzFx1BiFmJN0JraLPDJxIzbT0oihJ+aP8rv5HCKDP6WGen3QGeAGPPRFGMTnGBwAAAABJRU5ErkJggg==': '3',
+    'iVBORw0KGgoAAAANSUhEUgAAABYAAAAlCAAAAABjMUbkAAAAXUlEQVR4nM2RQQ7AIAgEB///5+1BogGxNr20ezKTETYBRqT5bpNChSmxwB7bUT7YSXasjZ3kjsMIlSuFHOd9twULeW9X8iiYYivCagy0da5/iOkNTrf8ENtvmrzBFzC4EkDfsOBbAAAAAElFTkSuQmCC': '4',
+    'iVBORw0KGgoAAAANSUhEUgAAABIAAAAnCAAAAAAnEkeVAAAAU0lEQVR4nNWQMRLAMAjDZC7//7I7tAMxGTq2bAjMcRJmL1USWG3ckbatGoQap36O1ORooodG0IYKMzhdeQYPt16+qo/4SuTXojOG1Lt75+B+RQ9cUcIMT7r4RuEAAAAASUVORK5CYII=': '5',
+    'iVBORw0KGgoAAAANSUhEUgAAABQAAAAnCAAAAAAqDDfSAAAAa0lEQVR4nMXSQQ6AIAxE0T/G+195XIDYFmLCRrvjwdCkqWhlRomzSOdBCuh0buiJ4KjwhlN6L/4jKg1ygeOixr380y2uSvWlAE/xZSME/nJKdZN2/0x5gw5lDaNzNMWl7aXFgolbnCy2fW4uOMQYQa9/DuEAAAAASUVORK5CYII=': '6',
+    'iVBORw0KGgoAAAANSUhEUgAAABUAAAAnCAAAAADFzlzsAAAAZElEQVR4nI3RSw7AIAgE0MH735kurJUy/FiRyRMSFCioZHEGqNzWYobZBFCqgHAaWgUkmBDZTUf2pRN76MB+tLeXttbQzlpqrbD9//8K3tfbHK2sp4UlmlumqQ1oZiOaWH/DXQ+NTRZHoU/n8gAAAABJRU5ErkJggg==': '7',
+    'iVBORw0KGgoAAAANSUhEUgAAABQAAAAnCAAAAAAqDDfSAAAAdklEQVR4nL2SQQoFMQhDX7z/nTMLS5mqA93830XBhwk2VuQxgFahTV5AJ0oSlWGQX17LSXEyBDiKGAENklATHDvvYZ3+Vm5QlEQMGlJKeNLxmR5HMiivo1Hhnmf3VIb8i+j+Bi92tKKr60DqahEtD33/+e21iweXTBxD3OtMtwAAAABJRU5ErkJggg==': '8',
+    'iVBORw0KGgoAAAANSUhEUgAAABQAAAAnCAAAAAAqDDfSAAAAbElEQVR4nN3SSRLAIAhE0d/c/86dRdQoYlWyDSt9ijhBD3s0dcPa10INIhkGdJsWCmZrq4Vna81YDQQOijihKixnusL3hfb82K5ppCfVofq++fwcE6aBPddfT/QfNCijQcXVaXzaJ0R6o552ASSIGD3UjG2FAAAAAElFTkSuQmCC': '9',
+    'iVBORw0KGgoAAAANSUhEUgAAABkAAAAcCAAAAACxT0sMAAAAbUlEQVR4nL2QSRbAIAhDE573vzJdOJRo6rKsCD8MSgBI9CBKsBKhxZZakQEdj1JshKt3Jy86yIq/SJtJWnJU5zQLEF8ADQn9vWmMHcgeC+4v9S2X245pWUk6gKDIYqLbQ3cblwpRw5Vv2iVl1wOqihc1Z7ZxSQAAAABJRU5ErkJggg==': 'a',
+    'iVBORw0KGgoAAAANSUhEUgAAABQAAAAnCAAAAAAqDDfSAAAAbklEQVR4nNWRSwoAIQxDX8rc/8qZhd+qG3czgiCPtMFEGAAxHXklQBwYcWDERj4E7f58GqpXQzlUrtCMwSrPyZUNkRgCXOCUUzfKqYOvfvSX5H2CFzvTvEFBpq2jmZY6tHmrRLcmXztSYrPtWPQCPboaSAvRw4wAAAAASUVORK5CYII=': 'b',
+    'iVBORw0KGgoAAAANSUhEUgAAABAAAAAcCAAAAABNWmDGAAAAQElEQVR4nN2RMQ4AIAgDr/7/z3XABEETdztAejQMIEJeHRUXwNsIhNMAyMXCoOkG9Ez8BPxMVJ1Hzh3K2h5FYRPHFwos/xI1SQAAAABJRU5ErkJggg==': 'c',
+    'iVBORw0KGgoAAAANSUhEUgAAABkAAAAnCAAAAADf8rxiAAAAc0lEQVR4nNWSwQ7AIAhDKfH/f5kd5ixtvHkw86Dia8BQEG3Ve6DtBhERkUawpE64/k7KyOBraa+GqKuxtDQM0Lq7QOs1KGS20ljqqHun/vgonGW7T9zMwzp7UxOKeE2Nmwa7v31zoJaKp4ymSsapTeHM/gDcwBdPSAdzDgAAAABJRU5ErkJggg==': 'd',
+    'iVBORw0KGgoAAAANSUhEUgAAABQAAAAcCAAAAABEscC8AAAAXklEQVR4nK3Ryw6AIAxE0TuN///L4wKoFlygsSs4mfJIYZSdSzWoexXqEJNhQM1UKACke7MJX7FsCaYSOGqw1ZJ8RAPyTvIV6vj+pH9w/dN+u/pYJhxnqmC9StTZJp5OiBIwqLub/QAAAABJRU5ErkJggg==': 'e',
+    'iVBORw0KGgoAAAANSUhEUgAAABQAAAAmCAAAAADhUOR3AAAAT0lEQVR4nM3SMQoAIAxD0R/x/lfWQdFaOjgU1EV5BJqCwpw2b3mweIS0TDtZApt42kA7xCYzUQTLVD8ZoLrQfrWofHLPHFSEbyv9hMDlr+sQ4gpHfFx6uwAAAABJRU5ErkJggg==': 'f'
+}
 
-def convertToBinary(img: Image):
+def convertToBinary(imgBase64: str):
+    img = Image.open(BytesIO(b64decode(imgBase64)))
     img = img.convert("RGB")
     px = np.array(img)
     for rows in range(px.shape[0]):
@@ -44,25 +54,29 @@ def extractCharacters(img: Image):
         object_cropped = object_extracted[y:y+h, x:x+w]
         object_pil = Image.fromarray(object_cropped)
         objects.append(object_pil)
+        for i, object in enumerate(objects):
+            object.save(f"{i}.png")
     return objects
 
 
 def read_captcha(imageBase64: str):
-    characters = getCharacters()
     imgBinary = convertToBinary(imageBase64)
     extractedCharacters = extractCharacters(imgBinary)
     captchaString = ""
     for extractedCharacter in extractedCharacters:
-        extractCharacterBase64 = b64encode(
-            extractedCharacter.tobytes()).decode("utf-8")
-        if extractCharacterBase64 in characters:
-            captchaString += characters[extractCharacterBase64]
+        buffered = BytesIO()
+        extractedCharacter.save(buffered, format="PNG")
+        extractedCharacterBase64 = b64encode(buffered.getvalue()).decode("utf-8")
+        if extractedCharacterBase64 in characters:
+            captchaString += characters[extractedCharacterBase64]
     return captchaString
 
 
 def mainFunction():
     image = Image.open("example.png")
-    print(getCharacters())
-    # print(read_captcha(image))
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    imgBase64 = b64encode(buffered.getvalue()).decode("utf-8")
+    print(read_captcha(imgBase64))
 
 mainFunction()
